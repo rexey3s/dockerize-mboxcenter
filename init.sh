773b32672f9fca5afc080e471d2cc24e10d705f2
rexey3s <rexey3s@gmail.com>
2016-05-23 12:27:02 +0700

    docker-compose.yml without jar App

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export CONFIG_DIR_PATH=/home/uit/ws/repo/dockerize-mboxcenter/config
+export MEDIDA_DIR_PATH=/home/uit/ws/repo/dockerize-mboxcenter/mcenter/ftproot/demo
+export ICECAST_PASSWORD=12345
+docker-compose up -d --force-recreate
+
