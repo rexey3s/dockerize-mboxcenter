020bec89cf648f1ca3ca37b375f1d1b12a26b122
rexey3s <rexey3s@gmail.com>
2016-12-22 11:29:39 +0700

    Ahihi - persistence redis/sql databse in two local dirs

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mkdir -p redisdb
+mkdir -p sqldb
 export CONFIG_DIR_PATH=/home/uit/ws/repo/dockerize-mboxcenter/config
 export MEDIDA_DIR_PATH=/home/uit/ws/repo/dockerize-mboxcenter/mcenter/ftproot/demo
 export ICECAST_PASSWORD=12345
